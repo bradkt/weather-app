f4feaf9bd94d6dbbb710ab21e9d4e2480b7a5d01
Brad <bradktracy@gmail.com>
2019-01-04 10:24:35 -0500

    Updates

---
+++ precache-manifest.89f19f7704215fe02f94afdadf4788c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af210fbb1e24644723dbe71312595034",
+    "url": "/react-app/static/media/movies-poster.af210fbb.jpg"
+  },
+  {
+    "revision": "64548ceb055035cfb25104856686e1f7",
+    "url": "/react-app/static/media/gray-world-map-dark-crop.64548ceb.png"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "c19a09bedc003bf619f3",
+    "url": "/react-app/static/js/main.c19a09be.chunk.js"
+  },
+  {
+    "revision": "e682252cd8b1a74e5058",
+    "url": "/react-app/static/js/1.e682252c.chunk.js"
+  },
+  {
+    "revision": "c19a09bedc003bf619f3",
+    "url": "/react-app/static/css/main.f54ba9c5.chunk.css"
+  },
+  {
+    "revision": "6af442c415a9ea403721c895f9084dba",
+    "url": "/react-app/index.html"
+  }
+];
